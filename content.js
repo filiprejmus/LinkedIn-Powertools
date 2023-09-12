0fbc44f4f716c032382e18f78c59b7defb083206
filiprejmus <rejmusfilip@gmail.com>
2023-09-12 19:29:41 +0200

    Some updates

--- content.js
+++ content.js
@@ -1,43 +1,47 @@
 // Function to inject the floating button
 function injectFloatingButton() {
-  const fontLink = document.createElement("link");
-  fontLink.href =
-    "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap";
-  fontLink.rel = "stylesheet";
-  document.head.appendChild(fontLink);
+  const container = document.createElement("div");
+  container.id = "cloudsquid-container";
+
   const button = document.createElement("button");
   button.id = "cloudsquid-button";
-  button.style.position = "fixed";
-  button.style.bottom = "20px";
-  button.style.right = "20px";
-  button.style.width = "50px";
-  button.style.height = "50px";
-  button.style.borderRadius = "50%";
-  button.style.backgroundColor = "#333";
-  button.style.color = "#fff";
-  button.style.border = "none";
-  button.style.zIndex = "999999";
-  button.innerText = "Fill";
-  button.style.fontFamily = "Montserrat, sans-serif";
+  button.innerText = "⋮"; // Using a vertical ellipsis as an indicator
+
+  const optionsList = document.createElement("ul");
+  optionsList.className = "cloudsquid-options-list";
+
+  const fillOption = document.createElement("li");
+  fillOption.innerText = "Outreach Message";
+
+  const followUp = document.createElement("li");
+  followUp.innerText = "Follow up - Step 1";
+
+  fillOption.onclick = outreachHandler;
+  followUp.onclick = followUpHandler;
+  optionsList.appendChild(fillOption);
+  optionsList.appendChild(followUp);
+
+  // Other options can be added similarly to fillOption
 
   button.onclick = function () {
-    const element = document.querySelector(
-      "h1.text-heading-xlarge.inline.t-24.v-align-middle.break-words"
-    );
-    const text = element.innerText;
-    const firstWord = text.split(" ")[0];
-    const msg = `Hey ${firstWord},
-I'm currently doing research about supply chain processes in ERPs for my startup. Would love to connect!`;
-    const textareaElem = document.querySelector(
-      "textarea[name='message'][id='custom-message']"
-    );
-    if (textareaElem) {
-      textareaElem.value = msg;
-      simulateUserInput(textareaElem); // Simulate a user input event
+    if (optionsList.style.display === "none") {
+      optionsList.style.display = "block";
+      button.style.borderRadius = "5%"; // Make it square
+      button.style.width = "50px"; // Adjust width as required
+      button.style.height = "auto";
+      button.style.marginBottom = "10px"; // Add a margin to separate the button from the list
+    } else {
+      optionsList.style.display = "none";
+      button.style.borderRadius = "50%"; // Back to circle
+      button.style.width = "50px";
+      button.style.height = "50px";
+      button.style.marginBottom = "0"; // Reset the margin
     }
   };
 
-  document.body.appendChild(button);
+  container.appendChild(button);
+  container.appendChild(optionsList);
+  document.body.appendChild(container);
 }
 
 // This function simulates user input to make sure the textarea acknowledges the changes.
@@ -45,6 +49,31 @@ function simulateUserInput(element) {
   const event = new Event("input", { bubbles: true, cancelable: true });
   element.dispatchEvent(event);
 }
+
+const outreachHandler = () => {
+  const element = document.querySelector(
+    "h1.text-heading-xlarge.inline.t-24.v-align-middle.break-words"
+  );
+  const text = element.innerText;
+  const firstWord = text.split(" ")[0];
+  const msg = `Hey ${firstWord},
+I'm currently doing research about supply chain processes for my startup. Would love to connect!`;
+  const textareaElem = document.querySelector(
+    "textarea[name='message'][id='custom-message']"
+  );
+  if (textareaElem) {
+    textareaElem.value = msg;
+    simulateUserInput(textareaElem); // Simulate a user input event
+  }
+};
+
+const followUpHandler = () => {
+  const messageBox = document.querySelector(".msg-form__contenteditable");
+  if (messageBox) {
+    messageBox.innerHTML = "<p>Thanks for accepting :)</p>";
+    simulateUserInput(messageBox); // Simulate a user input event
+  }
+};
 
 // On page load, inject the button.
 window.onload = injectFloatingButton;
